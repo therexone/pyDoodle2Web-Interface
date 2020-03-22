22d2b47fefca6b5fe8be09952aa3160562a4d263
therexone <renerjake@gmail.com>
2020-03-22 13:26:53 +0530

    initial commit

---
+++ pyD2W/form.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file = forms.FileField()
+
+
+def handle_uploaded_file(f):
+    with open('some/file/name.txt', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
