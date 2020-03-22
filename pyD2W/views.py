0f1a4bb73ff4e212fc95f6768e6350321f2b3854
therexone <renerjake@gmail.com>
2020-03-22 21:47:25 +0530

    Add error handling to upload interface

--- pyD2W/views.py
+++ pyD2W/views.py
@@ -1,6 +1,8 @@
 
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
+from .ocr import OCR
+from django.core.validators import validate_image_file_extension
 
 
 def home(request):
@@ -11,8 +13,14 @@ def upload(request):
     context = {}
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
-        fs = FileSystemStorage()
-        name = fs.save(uploaded_file.name, uploaded_file)
-        print(uploaded_file, uploaded_file.name)
-        context['url'] = fs.url(name)
+        try:
+            validate_image_file_extension(uploaded_file)
+            fs = FileSystemStorage()
+            name = fs.save(uploaded_file.name, uploaded_file)
+            context['url'] = fs.url(name)
+            tagsList = OCR(uploaded_file.name).readText()
+            
+        except Exception:
+            context['error'] = 'Error Occurred! Make sure the uploaded file is an Image'
+        
     return render(request, 'upload.html', context)
