19ea58a52f5b144ee72919b3c6733e23349dae02
therexone <renerjake@gmail.com>
2020-03-22 17:01:45 +0530

    Add ocr for uploaded file

---
+++ pyD2W/ocr.py
@@ -0,0 +1,22 @@
+try:
+    from PIL import Image
+except ImportError:
+    import PIL.Image
+import PIL
+import pytesseract
+import os
+
+
+image = os.path.join(os.path.abspath('../'), 'media', os.listdir('../media')[0])
+
+element_list = []
+
+def get_elements():
+    try:
+        elements = pytesseract.image_to_string(Image.open(image))
+        element_list = elements.split()
+        print(element_list)
+    except PIL.UnidentifiedImageError:
+        print('Could not read the image file, check filetype.')
+
+get_elements()
